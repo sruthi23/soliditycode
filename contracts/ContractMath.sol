28801d4542d30534a28ab5a6e69fbda98de810b1
sruthi23 <sruthi2525@gmail.com>
2018-06-07 11:32:14 +0530

    Sorting positive integer array -selection sort

--- contracts/ContractMath.sol
+++ contracts/ContractMath.sol
@@ -10,4 +10,45 @@ contract ContractMath{
 		else
 		return 1;
 	}
-}
+
+	/* function squarert(uint x)public pure returns (uint y) {
+
+		uint z = (x + 1) / 2;
+		y = x;
+		while (z < y) {
+			y = z;
+			z = (x / z + z) / 2;
+		}
+		return y;
+		} */
+
+		function sort_array(uint64[] arr_)public pure returns (uint64 [] )
+		{
+			uint256 l = arr_.length;
+			uint64[] memory arr = new uint64[] (l);
+
+			for(uint i=0;i<l;i++)
+			{
+				arr[i] = arr_[i];
+			}
+
+			for(i =0;i<l-1;i++)
+			{
+				for(uint j =i+1;j<l;j++)
+				{
+					if(arr[j]<arr[i])
+					{
+						uint64 temp= arr[i];
+						arr[i]=arr[j];
+						arr[j] = temp;
+
+					}
+
+				}
+			}
+
+			return arr;
+		}
+
+
+	}
